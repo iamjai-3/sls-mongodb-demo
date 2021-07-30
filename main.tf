768b7ecbbd6cae83c6b4973bad2490177987b1a1
iamjai-dev <jayaprakash@altrosyn.com>
2021-07-30 20:03:12 +0530

    implemented basic terraform

---
+++ main.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region                      = "ap-south-1"
+  access_key                  = "fake"
+  secret_key                  = "fake"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+  }
+}
+
+
+resource "aws_iam_role" "lambda_exec" {
+  name               = "serverless_example_lambda"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_lambda_function" "test" {
+  function_name = "getFunc"
+  s3_bucket     = "sls-test"
+  s3_key        = "apis.zip"
+  handler       = "apis/test.handler"
+  runtime       = "nodejs14.x"
+  role          = aws_iam_role.lambda_exec.arn
+}
+
+
+resource "aws_lambda_function" "test2" {
+  function_name = "getFunc2"
+  s3_bucket     = "sls-test"
+  s3_key        = "apis.zip"
+  handler       = "apis/apiHandler.getAll"
+  runtime       = "nodejs14.x"
+  role          = aws_iam_role.lambda_exec.arn
+}
+
