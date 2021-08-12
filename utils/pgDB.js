ce1ed95f5a36d02d511a6dcc7affc97455d203b8
iamjai-dev <jayaprakash@altrosyn.com>
2021-08-12 14:53:33 +0530

    updated minor changes

--- utils/pgDB.js
+++ utils/pgDB.js
@@ -5,4 +5,8 @@ const db = new Sequelize("sls_test", "postgres", "admin123", {
   dialect: "postgres",
 });
 
+// const db = new Sequelize(
+//   "postgres://postgres:admin123@192.168.1.105:5432/sls_test"
+// );
+
 module.exports = db;
